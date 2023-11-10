a661e7875a7725bb46610f466a7c952eabf4b79c
MDT-TEAM VHMing <132770113+mdt-vhming@users.noreply.github.com>
2023-11-10 23:49:23 +0700

    Add resources

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'VHMing - MDT-TEAM.XYZ'
+description 'MDT-JoinTransition | Hiệu ứng chuyển cảnh khi vào server'
+version '1.0.0'
+url 'https://github.com/mdt-vhming/mdt-jointransition'
+
+client_script {
+    'client.lua',
+}
+
+server_script {
+    'server.lua',
+}
+
+shared_scripts {
+    'config.lua',
+}
